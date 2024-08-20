ecc9e535d08f358b1dabc21f34670651abe57e80
SebasGalle07 <sebastian.gallegos@uqvirtual.edu.co>
2024-08-20 14:10:17 -0500

    sebas
    recordando python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/SebasGalle07.py
@@ -0,0 +1,18 @@
+
+
+#https://www.python.org
+
+#este comentario en linea
+"""hola bloque de comentario"""
+
+variable = "nuevaVariable"
+
+variable = "cambio valor"
+
+CONTANTE = "CONVENCION DE PROGRAMADOR"
+
+numerico:int = 5
+cadena:str = "cadena"
+booleano:bool = True #false
+
+print("Hola, python")
---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/SebasGalle07.py
@@ -0,0 +1,8 @@
+
+
+def sumar (num1, num2):
+    suma = num1+ num2
+    return suma
+    
+
+print("la suma de los valores es:" , sumar(4,5))
